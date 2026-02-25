69bb2be59127334af47ea60b07eaf676d58665a8
Andikasaktih <andikasaktih@gmail.com>
2026-02-25 10:44:33 +0700

    chore: add remaining config files

---
+++ config/activitylog.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    // Database connection untuk activity log
+    'database_connection' => null, // Gunakan default connection
+    
+    // Nama tabel untuk activity log
+    'table_name' => 'activity_log',
+    
+    // Nama kolom untuk primary key
+    'table_primary_key' => 'id',
+];
