493c63cb43da2a44e9bd28f91cf6a663ad389d2f
ghufronbagaskara <ghufronbagas@student.ub.ac.id>
2026-02-25 20:26:32 +0700

    feat: add HasApiTokens trait to User model for API token management

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,12 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+  use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * Kolom yang boleh diisi secara mass-assignment.
